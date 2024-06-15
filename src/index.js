2ab3d7323743b99286f85c4b32f856c941b18c12
Aosegir <128326345+Aosegir@users.noreply.github.com>
2024-06-15 13:47:12 -0700

    Search now returns the proper pieces of data from API.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-let main = document.getElementById('main');
+let content = document.getElementById('content');
+let search = document.getElementById('search');
+
+import './style.css';
 import { helloLocation } from './weather.js';
+
+let locationData = ['country', 'region', 'localtime', 'tz_id'];
+let currentData = ['temp_c', 'feelslike_c', 'wind_kph'];
 
 function searchLoader() {
     let input = document.createElement('input');
@@ -8,8 +14,62 @@ function searchLoader() {
     button.addEventListener('click', () => {
         helloLocation(input.value);
     });
-    main.appendChild(input);
-    main.appendChild(button);
+    search.appendChild(input);
+    search.appendChild(button);
 }
 
+function buttonLoader() {
+    let label = document.createElement('label');
+    label.classList.add('switch');
+    let input = document.createElement('input');
+    input.type = 'checkbox';
+    input.id = 'myToggle';
+    let span = document.createElement('span');
+    span.classList.add('slider', 'round');
+
+    input.addEventListener("change", function (event) {
+        if (event.target.checked) {
+            currentData = ['temp_f', 'feelslike_f', 'wind_mph'];
+        } else {
+            currentData = ['temp_c', 'feelslike_c', 'wind_kph'];
+        }
+    });
+
+    label.appendChild(input);
+    label.appendChild(span);
+    search.appendChild(label);
+};
+
+function locationDisplay(data) {
+    while(content.lastChild) {
+        content.removeChild(content.lastChild);
+    };
+    console.log(data);
+
+    for(let item of locationData) {
+        let dataElement = document.createElement('p');
+        dataElement.innerText = `${item}: ${data.location[item]}`;
+        content.appendChild(dataElement);
+    };
+
+    for(let item of currentData) {
+        let dataElement = document.createElement('p');
+        dataElement.innerText = `${item}: ${data.current[item]}`;
+        content.appendChild(dataElement);
+    };
+
+    let dataElement = document.createElement('p');
+    dataElement.innerText = `Condition: ${data.current.condition.text}`;
+    content.appendChild(dataElement);
+
+    /*for(let [key, value] of Object.entries(data.location)) {
+        let item = document.createElement('p');
+        item.innerText = `${key}: ${value}`;
+        content.appendChild(item);
+    };*/
+};
+
 searchLoader();
+buttonLoader();
+
+export { locationDisplay };
--- src/weather.js
+++ src/weather.js
@@ -1,13 +1,15 @@
 const API_KEY = 'df7eff5bf8be42a18c022953241406';
 const BASE_URL = 'http://api.weatherapi.com/v1';
 
+import { locationDisplay } from './index.js';
+
 async function helloLocation(location) {
     try {
         let data = await fetch(`${BASE_URL}/current.json?key=${API_KEY}&q=${location}`, {
             mode: 'cors'
         });
         let json_data = await data.json();
-        console.log(json_data.location);
+        locationDisplay(json_data);
     } catch {
         console.log("You fucked up!");
     };
